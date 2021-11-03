92c35a87ff659b05d4cdd598ac6946372b27c05d
shoupeng.wsp <shoupeng.wsp@1832276570603966.onaliyun.com>
2021-11-03 19:36:22 +0800

    模块简化

---
+++ generator/src/main/java/com/bamboo/generator/PkgAnnotation.java
@@ -0,0 +1,14 @@
+package com.bamboo.generator;
+
+import org.springframework.core.env.Environment;
+
+/**
+ * @author WuWei
+ * @date 2021/11/1 3:17 下午
+ */
+
+public class PkgAnnotation {
+
+
+}
+
---
+++ generator/src/main/java/com/bamboo/generator/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author WuWei
+ * @date 2021/11/1 3:09 下午
+ */
+package com.bamboo.generator;
